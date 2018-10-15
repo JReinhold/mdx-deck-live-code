9f9366d0f6e48260b51cbaae55380348b00174bb
Jeppe Reinhold <jeppereinhold@gmail.com>
2018-10-15 18:53:18 +0200

    add deeper props to LiveCode and LiveEditor

--- src/components/live-code.tsx
+++ src/components/live-code.tsx
@@ -1,21 +1,42 @@
 import * as React from 'react';
-import { LiveProvider, LiveError, LivePreview } from 'react-live';
-import { LiveEditor } from './live-editor';
+import {
+	LiveProvider,
+	LiveError,
+	LivePreview,
+	LiveProviderProps,
+} from 'react-live';
+import { LiveEditor, LiveEditorProps } from './live-editor';
+import { OmitRef } from '../types';
 
 interface LiveCodeProps {
 	code: string;
+	providerProps: OmitRef<LiveProviderProps>;
+	editorProps: OmitRef<LiveEditorProps>;
+	previewProps: OmitRef<React.HTMLProps<HTMLDivElement>>;
+	errorProps: OmitRef<React.HTMLProps<HTMLDivElement>>;
 }
 
-export const LiveCode: React.SFC<LiveCodeProps> = ({code}) => {
+/**
+ * A high level component to quickly add live coding abilities
+ */
+export const LiveCode: React.SFC<LiveCodeProps> = ({
+	code,
+	providerProps,
+	editorProps,
+	previewProps,
+	errorProps,
+}) => {
 	return (
-		<LiveProvider code={code}>
-			<LiveEditor />
-			<LivePreview />
-			<LiveError />
-		</LiveProvider>
+		<div>
+			<LiveProvider code={code} {...providerProps}>
+				<LiveEditor {...editorProps} />
+				<LivePreview {...previewProps} />
+				<LiveError {...errorProps} />
+			</LiveProvider>
+		</div>
 	);
 };
 
 LiveCode.defaultProps = {
-	code: '<h1>🙋‍♀️ 🌍</h1>'
-}
+	code: '<h1>🙋‍♀️ 🌍</h1>',
+};
--- src/components/live-editor.tsx
+++ src/components/live-editor.tsx
@@ -1,14 +1,29 @@
 import * as React from 'react';
-import { LiveEditor as LiveEditorBase } from 'react-live';
-
-interface LiveEditorState {
-	focusEditor: boolean;
-}
+import {
+	LiveEditor as BaseLiveEditor,
+	LiveEditorProps as BaseLiveEditorProps,
+} from 'react-live';
+import { OmitRef } from '../types';
 
 const isMacLike =
 	'navigator' in window && /(Mac|iPhone|iPod|iPad)/i.test(navigator.platform);
 
-export class LiveEditor extends React.PureComponent<{}, LiveEditorState> {
+interface LiveEditorState {
+	focusEditor: boolean;
+}
+export interface LiveEditorProps extends BaseLiveEditorProps {}
+/**
+ * A wrapper around the default LiveEditor from react-live
+ * Fixes focus state problems where mdx-deck and react-live don't get along
+ * maybe not needed when this PR is merged:
+ * https://github.com/FormidableLabs/react-live/pull/77
+ *
+ * Passes all valid params down to the LiveEditor from react-live
+ */
+export class LiveEditor extends React.PureComponent<
+	LiveEditorProps,
+	LiveEditorState
+> {
 	state = { focusEditor: true };
 
 	focusEditor = () => {
@@ -23,6 +38,8 @@ export class LiveEditor extends React.PureComponent<{}, LiveEditorState> {
 	 * - Escape
 	 * - CTRL + M (Windows and Linux)
 	 * - CTRL + SHIFT + M (Mac)
+	 * Mimicks Monaco edtor:
+	 * https://github.com/Microsoft/monaco-editor/wiki/Monaco-Editor-Accessibility-Guide#tab-trapping
 	 */
 	blurOnKeyCombo: React.KeyboardEventHandler<HTMLDivElement> = event => {
 		if (
@@ -37,6 +54,8 @@ export class LiveEditor extends React.PureComponent<{}, LiveEditorState> {
 
 	render() {
 		const { focusEditor } = this.state;
+		// we have to remove 'ref' prop to make TypeScript happy ¯\_(ツ)_/¯
+		const { ...rest } = this.props;
 
 		return (
 			<div
@@ -45,7 +64,10 @@ export class LiveEditor extends React.PureComponent<{}, LiveEditorState> {
 				onBlur={this.blurEditor}
 				onKeyDown={this.blurOnKeyCombo}
 			>
-				<LiveEditorBase contentEditable={focusEditor} />
+				<BaseLiveEditor
+					contentEditable={focusEditor}
+					{...rest as OmitRef<BaseLiveEditorProps>}
+				/>
 			</div>
 		);
 	}
